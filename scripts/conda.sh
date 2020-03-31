461b317c638a4132112f6d646b027cc9e412cce1
Tania Allard <taniar.allard@gmail.com>
2020-03-31 12:01:32 +0100

    Ensure we are using Python 3

--- scripts/conda.sh
+++ scripts/conda.sh
@@ -1,5 +1,6 @@
-conda env create -f environment.yml
+conda env create python=${{ matrix.python-version }} -f environment.yml
 conda info --envs
 eval "$(conda shell.bash hook)"
 conda activate opt-mo
+which python 
 python -m ipykernel install --user --name opt-mo
