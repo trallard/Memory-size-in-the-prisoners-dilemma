8bc5a4af20e6bcf5a9eab7f451c4299d5000ecf3
Tania Allard <taniar.allard@gmail.com>
2020-03-31 11:26:53 +0100

    Update CI

--- scripts/conda.sh
+++ scripts/conda.sh
@@ -2,4 +2,4 @@ conda env create -f environment.yml
 conda info --envs
 eval "$(conda shell.bash hook)"
 conda activate opt-mo
-python -m ipykernel install --name opt-mo
+python -m ipykernel install --user --name opt-mo
