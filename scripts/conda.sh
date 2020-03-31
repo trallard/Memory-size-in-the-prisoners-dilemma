8907bb324aa69fb60a7ea7a16b019f82784a3599
Tania Allard <taniar.allard@gmail.com>
2020-03-31 14:22:13 +0100

    :fire: Remove old script

--- scripts/conda.sh
+++
@@ -1,6 +0,0 @@
-conda env create python=${{ matrix.python-version }} -f environment.yml
-conda info --envs
-eval "$(conda shell.bash hook)"
-conda activate opt-mo
-which python 
-python -m ipykernel install --user --name opt-mo
